ee444508d90bdb6b799bf4eeacc34c43bf2bf9d9
gleiph <gleiph@fe80:0:0:0:5884:f1ff:fe24:e78c%8>
2014-12-01 00:02:19 -0800

    Adding AST resources

---
+++ src/main/java/br/uff/ic/github/mergeviewer/ast/Quickstart.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.github.mergeviewer.ast;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+/**
+ *
+ * @author gleiph
+ */
+public class Quickstart {
+
+    public static void main(String args[]) {
+        try {
+
+            ASTParser parser = ASTParser.newParser(AST.JLS3);
+//            File file = new File("/Users/gleiph/Dropbox/UCI/MergeViewer/src/main/java/br/uff/ic/github/mergeviewer/util/ContextFinder.java");
+//            File file = new File("/Users/gleiph/Dropbox/UCI/MergeViewer/src/main/java/br/uff/ic/github/mergeviewer/processing/Understanding.java");
+            File file = new File("/Users/gleiph/Dropbox/UCI/MergeViewer/src/test/java/br/uff/ic/github/mergeviewer/test/Structures.java");
+
+            String stringFile = FileUtils.readFileToString(file);
+            parser.setSource(stringFile.toCharArray());
+            parser.setKind(ASTParser.K_COMPILATION_UNIT);
+
+            final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+            cu.accept(new Visitor(cu));
+
+            List<Comment> commentList = cu.getCommentList();
+
+            for (Comment comment : commentList) {
+
+                comment.accept(new Visitor(cu));
+
+                int begin = cu.getLineNumber(comment.getStartPosition());
+                int end = cu.getLineNumber(comment.getStartPosition() + comment.getLength());
+
+                System.out.println("Comment(" + begin + ", " + end + ")");
+                System.out.println(comment.toString());
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Quickstart.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ src/main/java/br/uff/ic/github/mergeviewer/ast/Visitor.java
@@ -0,0 +1,361 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.github.mergeviewer.ast;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeMemberDeclaration;
+import org.eclipse.jdt.core.dom.BlockComment;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IExtendedModifier;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.Javadoc;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.SwitchCase;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.dom.ThrowStatement;
+import org.eclipse.jdt.core.dom.VariableDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.internal.compiler.ast.ForeachStatement;
+
+/**
+ *
+ * @author gleiph
+ */
+public class Visitor extends ASTVisitor {
+
+    private final CompilationUnit cu;
+
+    public Visitor(CompilationUnit cuArg) {
+        this.cu = cuArg;
+
+    }
+
+    private static void print(IExtendedModifier node) {
+    }
+
+    /*-----------------------------------------------------------------------
+    |                                  Tested                                |
+    -------------------------------------------------------------------------*/
+    
+    //ForStatement
+    @Override
+    public boolean visit(ForStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("ForStatement("+begin+", "+end+")");
+        System.out.println(node.toString());
+        return true;
+    }
+    
+    //If statement
+    @Override
+    public boolean visit(IfStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("IfStatement("+begin+", "+end+")");
+        System.out.println(node.toString());
+        return true;
+    }
+    
+    //Javadoc
+    @Override
+    public boolean visit(Javadoc node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+
+        System.out.println("Javadoc (" + begin + ", " + end + ")");
+        System.out.println(node.toString());
+        return true;
+    }
+    
+    //Method declaration
+    @Override
+    public boolean visit(MethodDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("MethodDeclaration("+begin+", "+end+")");
+        System.out.println(node.toString());
+
+        return true;
+    }
+    
+    //While statement
+    @Override
+    public boolean visit(WhileStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("WhileStatement("+begin+", "+end+")");
+        System.out.println(node.toString());
+        return true;
+    }
+    
+    //VariableDeclarationExpression
+    //int i=0
+    @Override
+    public boolean visit(VariableDeclarationExpression node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("VariableDeclarationExpression (" + begin + ", " + end + ")");
+        System.out.println(node.toString());
+        return true;
+    }
+    
+    /*-----------------------------------------------------------------------
+    |                                  Untested                                |
+    -------------------------------------------------------------------------*/
+    
+    //Annotation
+    public boolean visit(Annotation node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+
+        System.out.println("Annotation(" + begin + ", " + end + ")");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(AnnotationTypeDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("AnnotationTypeDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(AnnotationTypeMemberDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("AnnotationTypeMemberDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Annotation element
+    //Attribute
+    //private JProgressBar jProgressBar;
+    @Override
+    public boolean visit(FieldDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("FieldDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(FieldAccess node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("FieldAccess");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Blank
+    //Class declaration
+    //Class signature
+    //Comment
+    public boolean visit(Comment node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("Comment");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(BlockComment node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+
+        System.out.println("BlockComment (" + begin + ", " + end + ")");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    
+    
+
+    //Enum
+    @Override
+    public boolean visit(EnumConstantDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("EnumConstantDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(EnumDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("EnumDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    
+
+    public boolean visit(ForeachStatement node) {
+        int begin = cu.getLineNumber(node.sourceStart());
+        int end = cu.getLineNumber(node.sourceEnd());
+        System.out.println("ForeachStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(EnhancedForStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("EnhancedForStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    
+
+    //Import
+    @Override
+    public boolean visit(ImportDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("ImportDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Instance initializer
+    @Override
+    public boolean visit(Initializer node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("Initializer("+begin+", "+end+")");
+        System.out.println(node.toString());
+
+        //Put here the diference between the diferent kinds of initialization 
+        
+        return true;
+    }
+    
+    
+
+    //Method invocation
+    @Override
+    public boolean visit(MethodInvocation node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("MethodInvocation");
+        System.out.println(node.toString());
+
+        return true;
+    }
+
+    //Method interface
+    //Method signature
+//    public boolean visit(MethodSignature node) {
+//        System.out.println("MethodSignature");
+//        System.out.println(node.toString());
+//        return true;
+//    }
+
+
+    //Return statement
+    @Override
+    public boolean visit(ReturnStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("ReturnStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Static initialization block
+    //Switch case statement
+    @Override
+    public boolean visit(SwitchCase node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("SwitchCase");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(SwitchStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("SwitchStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Throw statement
+    @Override
+    public boolean visit(ThrowStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("ThrowStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Try statement 
+    //Variable 
+    public boolean visit(VariableDeclaration node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("VariableDeclaration");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //VariableDeclarationExpression (tested)
+    
+
+    //No assiging value
+    //Subset of VariableDeclarationExpression
+    @Override
+    public boolean visit(VariableDeclarationFragment node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("VariableDeclarationFragment");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    //Declaration itself 
+    @Override
+    public boolean visit(VariableDeclarationStatement node) {
+        int begin = cu.getLineNumber(node.getStartPosition());
+        int end = cu.getLineNumber(node.getStartPosition() + node.getLength());
+        System.out.println("VariableDeclarationStatement");
+        System.out.println(node.toString());
+        return true;
+    }
+
+    
+
+}
