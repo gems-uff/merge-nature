a49339bd9a1b93d85fe6c23e5c0bb422786b6a8b
gleiph <gleiph@gleiphs-mbp.ic.uff.br>
2015-06-02 18:12:38 -0300

    Moving classes Project and Language to Resources

---
+++ resources/src/main/java/br/uff/ic/gems/resources/data/Language.java
@@ -0,0 +1,75 @@
+package br.uff.ic.gems.resources.data;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Gleiph
+ */
+
+@Entity
+public class Language implements Serializable{
+ 
+    @Id
+    @GeneratedValue
+    Long id;
+    
+    private String name;
+    private double size;
+    private double percentage;
+
+    public Language() {
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the size
+     */
+    public double getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public Language(String name, double size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    /**
+     * @return the percentage
+     */
+    public double getPercentage() {
+        return percentage;
+    }
+
+    /**
+     * @param percentage the percentage to set
+     */
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+    
+    
+}
