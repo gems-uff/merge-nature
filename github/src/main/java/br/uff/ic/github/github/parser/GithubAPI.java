af80ca8a229c6c5420ae4367a228ee54fca6a375
gleiph <gleiph@192.168.0.101>
2015-06-07 03:37:37 -0300

    Removing unused import

--- github/src/main/java/br/uff/ic/github/github/parser/GithubAPI.java
+++ github/src/main/java/br/uff/ic/github/github/parser/GithubAPI.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
