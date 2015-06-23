cf04cd92e567ba0f9f57a372d11cd97f8a2a9280
gleiph <gleiph@192.168.0.103>
2015-06-23 04:48:27 -0300

    Improving visualization

--- projectViewer/src/main/java/br/uff/ic/kraken/projectviewer/bean/ShowConflictsBean.java
+++ projectViewer/src/main/java/br/uff/ic/kraken/projectviewer/bean/ShowConflictsBean.java
@@ -5,8 +5,12 @@
  */
 package br.uff.ic.kraken.projectviewer.bean;
 
+import br.uff.ic.gems.resources.data.LanguageConstruct;
 import br.uff.ic.kraken.projectviewer.utils.Navigation;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 
@@ -51,5 +55,27 @@ public class ShowConflictsBean extends Navigation implements Serializable {
     
     public String navigate(){
         return super.dataNavigation(id, dataType);
+    }
+    
+    public List<String> getGeneralKindOfConflict(List<LanguageConstruct> leftFiltered, List<LanguageConstruct> rightFiltered){
+        
+        if(leftFiltered == null || rightFiltered == null)
+            return new ArrayList<>();
+        
+        List<String> result = new ArrayList<>();
+        
+        for (LanguageConstruct lf : leftFiltered) {
+            if(!result.contains(lf.getName()))
+                result.add(lf.getName());
+        }
+        
+        for (LanguageConstruct rf : rightFiltered) {
+            if(!result.contains(rf.getName()))
+                result.add(rf.getName());
+        }
+
+        Collections.sort(result);
+        
+        return result;
     }
 }
