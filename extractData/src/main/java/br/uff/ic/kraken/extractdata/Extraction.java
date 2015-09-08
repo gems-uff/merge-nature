96a051916bee1abf0f16c00fceb4f90ff680f80b
gleiph <gleiph@192.168.0.100>
2015-09-08 11:33:12 -0300

    Continuing analyses

--- extractData/src/main/java/br/uff/ic/kraken/extractdata/Extraction.java
+++ extractData/src/main/java/br/uff/ic/kraken/extractdata/Extraction.java
@@ -6,12 +6,17 @@
 package br.uff.ic.kraken.extractdata;
 
 import br.uff.ic.gems.resources.analises.merge.AutomaticAnalysis;
+import br.uff.ic.gems.resources.data.Revision;
 import br.uff.ic.gems.resources.jpa.DatabaseManager;
 import br.uff.ic.gems.resources.utils.FileManager;
 import java.io.File;
+import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -49,6 +54,83 @@ public class Extraction {
 
         } else {
             System.out.println("java -jar AutomaticAnalysis repositoriesDirectoryPath projectURL outputDirectoryPath");
+        }
+    }
+//    public static void main(String[] args) {
+////        preProcessingProject("/Users/gleiph/Desktop/teste/out/processing-js");
+//        preProcessingProject(args[0]);
+//    }
+//
+    private static void preProcessingProject(String pathProjectOutput) {
+
+        List<String> SHAs = null;
+
+        if (!pathProjectOutput.endsWith(File.separator)) {
+            pathProjectOutput += File.separator;
+        }
+
+        String pathSHA = pathProjectOutput+"sha";
+        try {
+
+            boolean error = false;
+
+            SHAs = FileUtils.readLines(new File(pathSHA));
+
+            int currentRevision = 1;
+
+            File projectDir = new File(pathProjectOutput);
+            List<File> subdirectories = new ArrayList<>();
+
+            String projectName = projectDir.getName();
+
+            for (File file : projectDir.listFiles()) {
+                if (file.isDirectory()) {
+                    subdirectories.add(file);
+                }
+            }
+
+            List<String> SHAConfirmed = new ArrayList<>();
+
+            for (int i = 0; i < subdirectories.size(); i++) {
+
+                File subdirectory = new File(projectDir, i + "");
+
+                String[] jsonFiles = subdirectory.list();
+
+                for (int j = 0; j < jsonFiles.length; j++) {
+
+                    File jsonFile = new File(subdirectory, projectName + currentRevision);
+
+                    Revision revision = AutomaticAnalysis.readRevision(jsonFile.getAbsolutePath());
+
+                    if (!SHAs.get(currentRevision - 1).equals(revision.getSha())) {
+//                        System.out.println("Error!!!! "+ currentRevision + " - " + revision.getSha());
+                        error = true;
+                        break;
+                    } else {
+//                        System.out.println("OK!!!! "+ currentRevision + " - " + revision.getSha());
+                        SHAConfirmed.add(revision.getSha());
+                    }
+                    currentRevision++;
+                }
+
+                if (error) {
+                    break;
+                }
+            }
+
+            if (!SHAConfirmed.equals(SHAs)) {
+                System.out.println("Error");
+                System.out.println("currentRevision = " + currentRevision);
+                System.out.println("SHAConfirmedSize = " + SHAConfirmed.size());
+                System.out.println("SHAs size = " + SHAs.size());
+                System.out.println("Current SHA = " + SHAConfirmed.get(SHAConfirmed.size() - 1));
+            } else {
+                System.out.println("OK!!!");
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Extraction.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
