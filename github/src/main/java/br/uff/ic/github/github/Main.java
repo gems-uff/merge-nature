000850736e32babf3ec8da3b5ca40d35a0c060d5
Gleiph <gleiphgh@gmail.com>
2015-12-11 21:56:02 -0200

    Extracting fork information

--- github/src/main/java/br/uff/ic/github/github/Main.java
+++ github/src/main/java/br/uff/ic/github/github/Main.java
@@ -1,6 +1,8 @@
 package br.uff.ic.github.github;
 
+import br.uff.ic.gems.resources.data.dao.sql.JDBCConnection;
 import br.uff.ic.gems.resources.github.parser.GithubAPI;
+import java.sql.Connection;
 
 /**
  *
@@ -10,8 +12,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GithubAPI.init();
-        GithubAPI.projects();
+        String database = "apagarLogo";
+        
+        try (Connection connection = (new JDBCConnection()).getConnection(database)) {
+
+            GithubAPI.init();
+            GithubAPI.projects(connection);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
