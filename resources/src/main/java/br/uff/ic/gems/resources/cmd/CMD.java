2ccb20252bdfb3c2c630f7bdb18950308a3d6887
Gleiph <gleiphgh@gmail.com>
2016-01-26 02:48:38 -0200

    Creating CMD to array

--- resources/src/main/java/br/uff/ic/gems/resources/cmd/CMD.java
+++ resources/src/main/java/br/uff/ic/gems/resources/cmd/CMD.java
@@ -54,6 +54,43 @@ public class CMD {
 
         return result;
     }
+    
+    public static CMDOutput cmdArray(String path, String[] command) {
+
+        CMDOutput result = new CMDOutput();
+
+        try {
+
+            Process exec;
+
+            if (path != null) {
+                exec = Runtime.getRuntime().exec(command, null, new File(path));
+            } else {
+                exec = Runtime.getRuntime().exec(command);
+            }
+
+            String s;
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(exec.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(exec.getErrorStream()));
+
+            // read the output from the command
+            while ((s = stdInput.readLine()) != null) {
+                result.addOutput(s);
+            }
+
+            // read any errors from the attempted command
+            while ((s = stdError.readLine()) != null) {
+                result.addErrors(s);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(CMD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return result;
+    }
 
     public static CMDOutput cmd(String path, String command, boolean showProgress) {
 
